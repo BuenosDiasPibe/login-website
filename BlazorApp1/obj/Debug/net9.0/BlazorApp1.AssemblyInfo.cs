abe6dff61c66abc00d7c3d51766ab8ef9059473e
BuenosDiasPibe <juliabarrios782@gmail.com>
2025-05-01 19:28:51 -0300

    implementado sistema de login eliminado counter.razor cambiado home.razor añadido 2 nuevas dependencias, ASPNet tools y PostgreSQL modificado varios elementos de css, no se nota en el usuario final pero shhhhhh

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687d0cc7e743b6c74604b73400ee96011ce7b343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1917d8e8e94884d84813f125a8f8f9711691c9de")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
